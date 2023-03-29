3e1a0ae4c2d75c3993e62d8e55a643adda6adb83
Nujsvart <kampfar@gmail.com>
2023-03-29 03:18:36 +0300

    react-slick ile slider tamamlandi

--- components/ui/Caraousel.jsx
+++ components/ui/Caraousel.jsx
@@ -1,7 +1,27 @@
 import Image from "next/image";
 import Title from "./Title";
 
+import Slider from "react-slick";
+
 const Caraousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autopalySpeed: 3500,
+    appendDots: dots => (
+      <div>
+        <ul className="relative">{dots}</ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div className="w-3 h-3 border bg-white rounded-full"></div>
+    ),
+  };
   return (
     <div className="relative w-full h-screen">
       <Image
@@ -10,16 +30,33 @@ const Caraousel = () => {
         objectFit="cover"
         alt="caraousel-bg"
       />
-      <div className="container relative flex flex-col items-start justify-center h-full mx-auto text-white z-99 gap-y-7">
-        <Title className="font-bold text-7xl">Fast Food Restaurant</Title>
-        <p className="sm:w-2/3 w-full text-2xl font-light">
-          Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente
-          ad mollitia laborum quam quisquam esse error unde. Tempora ex
-          doloremque, labore, sunt repellat dolore, iste magni quos nihil
-          ducimus libero ipsam.
-        </p>
-        <button className="btn-primary font-bold  mt-3">Order Now</button>
-      </div>
+      <Slider {...settings}>
+        <div className="h-screen">
+          <div className="container relative flex flex-col items-start justify-center h-full mx-auto text-white z-99 gap-y-7 ">
+            <Title className="font-bold text-7xl">Fast Food Restaurant</Title>
+            <p className="sm:w-2/3 w-full text-2xl font-light">
+              Doloremque, itaque aperiam facilis rerum, commodi, temporibus
+              sapiente ad mollitia laborum quam quisquam esse error unde.
+              Tempora ex doloremque, labore, sunt repellat dolore, iste magni
+              quos nihil ducimus libero ipsam.
+            </p>
+            <button className="btn-primary font-bold  mt-3">Order Now</button>
+          </div>
+        </div>
+
+        <div className="h-screen">
+          <div className="container relative flex flex-col items-start justify-center h-full mx-auto text-white z-99 gap-y-7 ">
+            <Title className="font-bold text-7xl">Fast Food Restaurant</Title>
+            <p className="sm:w-2/3 w-full text-2xl font-light">
+              Doloremque, itaque aperiam facilis rerum, commodi, temporibus
+              sapiente ad mollitia laborum quam quisquam esse error unde.
+              Tempora ex doloremque, labore, sunt repellat dolore, iste magni
+              quos nihil ducimus libero ipsam.
+            </p>
+            <button className="btn-primary font-bold  mt-3">Order Now</button>
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 };

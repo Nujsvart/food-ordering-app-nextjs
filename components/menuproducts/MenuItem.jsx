a6a55ba8c26eabe80035db7ed5e63214d7ebae71
Nujsvart <kampfar@gmail.com>
2023-04-06 10:29:00 +0300

    redux-toolkit oncesi

--- components/menuproducts/MenuItem.jsx
+++ components/menuproducts/MenuItem.jsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 import { FaShoppingCart } from "react-icons/fa";
+import Link from "next/link";
 
 const MenuItem = () => {
   return (
     <div className="flex flex-col box bg-secondary rounded-xl overflow-hidden ">
-      <div className="bg-[#f1f2f3] rounded-b-3xl flex justify-center relative ">
-        <Image
-          src="/images/f1.png"
-          width={160}
-          height={160}
-          alt="pizza"
-          className="py-5 hover:scale-105 transition-all duration-500"
-        />
-      </div>
+      <Link href="/product">
+        <div className="bg-[#f1f2f3] rounded-b-3xl flex justify-center relative ">
+          <Image
+            src="/images/f1.png"
+            width={160}
+            height={160}
+            alt="pizza"
+            className="py-5 hover:scale-105 transition-all duration-500"
+          />
+        </div>
+      </Link>
+
       <div className="p-5 flex flex-col gap-y-5   ">
         <h4 className="font-bold">Delicious Pizza</h4>
         <p className="text-sm">
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Title from "@/components/ui/Title";
 
 const Index = () => {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [productInfo, setProductInfo] = useState({
+    products: [
+      {
+        size: "Small",
+        width: 40,
+        price: 10,
+      },
+      {
+        size: "Medium",
+        width: 50,
+        price: 20,
+      },
+      {
+        size: "Large",
+        width: 60,
+        price: 30,
+      },
+    ],
+
+    extras: [
+      {
+        name: "Ketcap",
+        price: 10,
+        checked: false,
+      },
+      {
+        name: "Mayonez",
+        price: 20,
+        checked: false,
+      },
+      {
+        name: "AciSos",
+        price: 30,
+        checked: false,
+      },
+    ],
+    totalPrice: 10,
+  });
+
   return (
     <div className="container mx-auto">
       <div className="flex mt-20 gap-20 sm:flex-row flex-col h-screen sm:h-auto">
@@ -17,7 +57,7 @@ const Index = () => {
         <div className="flex-1 flex justify-center gap-y-4 flex-col items-start">
           <Title className="text-3xl">Good Pizza</Title>
           <span className="text-primary text-2xl font-bold underline underline-offset-4">
-            $10
+            ${productInfo.totalPrice}
           </span>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
@@ -28,73 +68,36 @@ const Index = () => {
           <div className="grid gap-y-4">
             <h4 className="font-bold">Choose the size</h4>
             <div className="flex gap-x-14 items-center">
-              <div className="relative">
-                <Image
-                  src="/images/pizzasize.png"
-                  width={40}
-                  height={40}
-                  alt="pizza"
-                />
-                <span className="absolute -top-2 -right-6 text-xs bg-primary rounded-full px-3 py-0.5 font-medium">
-                  Small
-                </span>
-              </div>
-              <div className="relative">
-                <Image
-                  src="/images/pizzasize.png"
-                  width={50}
-                  height={50}
-                  alt="pizza"
-                />
-                <span className="absolute -top-2 -right-6 text-xs bg-primary rounded-full px-3 py-0.5 font-medium">
-                  Medium
-                </span>
-              </div>
-              <div className="relative">
-                <Image
-                  src="/images/pizzasize.png"
-                  width={60}
-                  height={60}
-                  alt="pizza"
-                />
-                <span className="absolute -top-2 -right-6 text-xs bg-primary rounded-full px-3 py-0.5 font-medium">
-                  Large
-                </span>
-              </div>
+              {productInfo.products.map((item, index) => (
+                <div className="relative cursor-pointer" key={index}>
+                  <Image
+                    src="/images/pizzasize.png"
+                    width={item.width}
+                    height={item.width}
+                    alt={item.size}
+                  />
+                  <span className="absolute -top-2 -right-6 text-xs bg-primary rounded-full px-3 py-0.5 font-medium">
+                    {item.size}
+                  </span>
+                </div>
+              ))}
             </div>
           </div>
           <div className="grid gap-y-4">
             <h4 className="font-bold">Choose additional ingredients</h4>
             <div className="flex gap-x-4">
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="ketcap"
-                  id="ketcap"
-                  className="accent-primary mr-2"
-                />
-                <span className="text-sm font-semibold">Ketcap</span>
-              </label>
-
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="mayonez"
-                  id="mayonez"
-                  className="accent-primary mr-2"
-                />
-                <span className="text-sm font-semibold">Mayonez</span>
-              </label>
-
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="acisos"
-                  id="acisos"
-                  className="accent-primary mr-2"
-                />
-                <span className="text-sm font-semibold">Aci Sos</span>
-              </label>
+              {productInfo.extras.map((item, index) => (
+                <label className="flex items-center" key={index}>
+                  <input
+                    type="checkbox"
+                    name={item.name}
+                    id={item.name}
+                    checked={item.checked}
+                    className="accent-primary mr-2"
+                  />
+                  <span className="text-sm font-semibold">{item.name}</span>
+                </label>
+              ))}
             </div>
           </div>
           <button className="btn-primary">Add to Cart</button>
